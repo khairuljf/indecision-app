db5c14efe038e759214548ff39ec2eb203b55df5
Khairul Islam <khairul@orangetoolz.com>
2021-04-08 16:32:28 +0600

    First jsx by manual

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,6 @@
 console.log("Helllo world");
+
+//var template = <p>This is JSX from app.js</p>;
+var template = /*#__PURE__*/React.createElement("h1", null, "Welcome to React ");
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot)
