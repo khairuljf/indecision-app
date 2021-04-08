5f2547321e9b0e1f8ec49d308d4620942a9fd7e2
Khairul Islam <khairul@orangetoolz.com>
2021-04-08 23:02:32 +0600

    Success fully compile jsx to reactDOM

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
-'use strict';
-
 console.log('App.js is running now!');
 
 // JSX - JavaScript XML
-var template = React.createElement(
-  'h1',
-  { id: 'heading' },
-  'Indecision App!'
-);
+var template = <h1 id="heading">Indecision App!</h1>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
