a64441c17f587a97fb1843948fe7054ebc9d5722
Khairul Islam <khairul@orangetoolz.com>
2021-04-09 06:54:29 +0600

    Content show by varibale with js object

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,62 @@ console.log('App.js is running now!');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
-  { id: 'heading' },
-  'Indecision App!'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Hello Khairul'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'App Description will be here'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'List item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'List item 2'
+        )
+    )
+);
+
+var user = {
+    name: 'Khairul Islam',
+    age: 25,
+    location: 'Chapainaabgonj'
+};
+
+var templateTwp = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Name : ',
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age : ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location : ',
+        user.location
+    )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwp, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
 console.log('App.js is running now!');
 
 // JSX - JavaScript XML
-var template = <h1 id="heading">Indecision App!</h1>;
+var template = (
+    <div>
+        <h1>Hello Khairul</h1>
+        <p>App Description will be here</p>
+        <ol>
+            <li>List item 1</li>
+            <li>List item 2</li>
+        </ol>
+    </div>
+);
+
+var user ={
+    name:'Khairul Islam',
+    age:25,
+    location:'Chapainaabgonj'
+}
+
+var templateTwp = (
+    <div>
+        <h1>Name : {user.name}</h1>
+        <p>Age : {user.age}</p>
+        <p>Location : {user.location}</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwp, appRoot);
