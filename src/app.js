d3dc5398a6d79f437b3afe4e0465c5914069b565
Khairul Islam <khairul@orangetoolz.com>
2021-04-15 21:44:50 +0600

    Js import & export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log("App.js is running!") 
+
+import './utils.js'
+
+import {isAdult, canDrink} from './person.js';
+
+console.log(isAdult(25)) 
+
+console.log(canDrink(17)) 
---
+++ src/person.js
@@ -0,0 +1,5 @@
+const isAdult = (age) => age>=18
+
+const canDrink = (age) => age>=21;
+
+export { isAdult, canDrink }
