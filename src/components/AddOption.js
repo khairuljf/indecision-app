c94c0e4bbec7845fdea2901da0fa9489c3e6a53d
Khairul Islam <khairul@orangetoolz.com>
2021-04-17 10:00:03 +0600

    Add trasnform class propertis for remove constructor from class

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
 export default class AddOption extends React.Component{
-    constructor(props){
-        super(props);
-        this.formSubmit= this.formSubmit.bind(this);
-        this.state ={
-            error:undefined
-        }
+    state = {
+        error:undefined
     }
-
-    formSubmit(e){
+    formSubmit= (e) =>{
         e.preventDefault();
         const option = e.target.elements.option.value.trim();
        const error =  this.props.handleAddOption(option)
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -6,16 +6,39 @@ import Action from './Action';
 
 export default class IndecisionApp extends React.Component{
 
-    constructor(props){
-        super(props)
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this)
-        this.handleAction = this.handleAction.bind(this)
-        this.state ={
-            options:[]
-        }
+    state ={
+        options:[]
     }
+
+    
+    handleDeleteOptions = () =>{ 
+        this.setState( ()=>({options:[]}) )
+    }
+
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState)=>({
+            options:prevState.options.filter((option)=> optionToRemove !== option )
+        }))
+    }
+
+    handleAction = () =>{
+        const RandomNumber = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[RandomNumber]
+      console.log(option)
+    }
+
+    handleAddOption = (option) => {
+
+        if(!option){
+            return 'Enter Valid Value item';
+        }else if(this.state.options.indexOf(option)>-1){
+            return 'This option alreay exits';
+        }
+        this.setState((prevSate)=>({options:prevSate.options.concat([option])}))
+    }
+ 
+
+
 
     componentDidMount(){
 
@@ -44,31 +67,6 @@ export default class IndecisionApp extends React.Component{
         console.log("Data Saving")
     }
 
-    handleDeleteOptions(){ 
-        this.setState( ()=>({options:[]}) )
-    }
-
-    handleDeleteOption(optionToRemove){
-        this.setState((prevState)=>({
-            options:prevState.options.filter((option)=> optionToRemove !== option )
-        }))
-    }
-
-    handleAction(){
-        const RandomNumber = Math.floor(Math.random() * this.state.options.length);
-        const option = this.state.options[RandomNumber]
-      console.log(option)
-    }
-
-    handleAddOption(option){
-
-        if(!option){
-            return 'Enter Valid Value item';
-        }else if(this.state.options.indexOf(option)>-1){
-            return 'This option alreay exits';
-        }
-        this.setState((prevSate)=>({options:prevSate.options.concat([option])}))
-    }
 
 
  render(){
